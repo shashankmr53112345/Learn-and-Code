a8904f8c4930bddd5d72adce38b040409642faa8
Shashank MR <shashank.mr@intimetec.com>
2025-03-12 00:24:11 +0530

    Removing all files

--- LearnAndCodeAssignment_Week2/BookServices/HtmlPrinter.java
+++
@@ -1,6 +0,0 @@
-
-public class HtmlPrinter {
-    public void printPage(String pageContent) {
-        System.out.println("<div style='single-page'>" + pageContent + "</div>");
-    }
-}
--- LearnAndCodeAssignment_Week2/BookServices/PlainTextPrinter.java
+++
@@ -1,6 +0,0 @@
-
-public class PlainTextPrinter {
-    public void printPage(String pageContent) {
-        System.out.println(pageContent);
-    }
-}
